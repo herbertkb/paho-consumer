394943009b25e4df2c2e670f86447e76e83b8eb2
Keith Herbert <kherbert@redhat.com>
2025-01-30 18:01:15 -0500

    convert project to Spring Boot

--- src/main/java/com/example/paho/consumer/ConsumerRouteBuilder.java
+++ src/main/java/com/example/paho/consumer/ConsumerRouteBuilder.java
@@ -1,15 +1,17 @@
-package com.example.routes;
+package com.example.paho.consumer;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 import static org.apache.camel.LoggingLevel.INFO;
 
+@Component
 public class ConsumerRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
         from("paho-mqtt5:{{topic}}"
-                +"?brokerUrl={{mqtt-servers}}"
-                +"&clientId={{clientId}}"
+                +"?brokerUrl={{broker.url}}"
+                +"&clientId={{client.id}}"
                 +"&qos=1")
             .id("paho-consumer")
             .log(INFO, "Received: ${body}");
